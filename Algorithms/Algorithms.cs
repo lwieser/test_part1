b2c53cd11c6685968a9142b6967afc8124b23195
Laurent <lwieser@capcod.eu>
2020-10-27 10:43:37 +0100

    start exercices

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -10,5 +10,23 @@ namespace Algorithms
 
             return (float)a / (float)b;
         }
+
+        public static int[] Fibonacci(int length)
+        {
+            return null;
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+            return false;
+        }
+        public static int PGCD(string word)
+        {
+            return 0;
+        }
+        public static string CorrectText(string text)
+        {
+            return null;
+        }
     }
 }
