0b99327112763b122ead9a4e9356e9cedc27c9a5
Laurent <lwieser@capcod.eu>
2020-10-27 06:51:15 +0100

    Ajoutez des fichiers projet.

---
+++ Test_Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
